86ff4487d18673cda68fc6133160169526607a3e
epulla <erickpulladsi@gmail.com>
2022-10-24 02:00:18 -0500

    refactor: NotNotFound exception

--- backend/app/note/domain/exceptions.py
+++ backend/app/note/domain/exceptions.py
@@ -2,4 +2,7 @@
 
 class NoteNotFound(Exception):
     """Raised when a note is not found in the DB"""
-    pass
+
+    def __init__(self):
+        self.message = "Note not found"
+        super().__init__(self.message)
